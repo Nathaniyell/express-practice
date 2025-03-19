ec89ef290376eb06b912a92d4fbfeefe6cff3ea8
Nathaniyell <essien.nathan@yahoo.com>
2025-03-19 17:10:17 +0100

    restructure validation logic for course

--- server.js
+++ server.js
@@ -43,12 +43,7 @@ app.get('/api/courses/:id', (req, res) => {
 
 
 app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    const { error } = schema.validate(req.body);
-    
+    const { error } = validateCourse(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
@@ -63,29 +58,28 @@ app.post('/api/courses', (req, res) => {
 });
 
 
-app.put('/api/courses/:id', (req,res)=>{
-    const course = courses.find(c => c.id === parseInt(req.params.id))
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) {
-        res.status(404).send("The course with the specified ID was not found")
-        return
+        return res.status(404).send("The course with the specified ID was not found");
     }
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
 
-    const { error } = schema.validate(req.body);
-    
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-    course.name = req.body.name
-    res.send(course);
-})
-
-
-
-
-
+    return schema.validate(course);  
+}
 
 
 
