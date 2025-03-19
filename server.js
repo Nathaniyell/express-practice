1684a655e2008967d05f6b142346e5e747a2188d
Nathaniyell <essien.nathan@yahoo.com>
2025-03-19 13:14:45 +0100

    add new api route for courses

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.get('/', (req, res)=>{
     res.send("Hello World")
 })
 
+app.get('/api/courses', (req, res)=>{
+    res.send([1,2,3])
+})
 
 app.listen(PORT, ()=>{
     console.log("App is running on port: " + PORT)
