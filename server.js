74480a32921e53924e675b55690539b885a3644f
Nathaniyell <essien.nathan@yahoo.com>
2025-03-19 14:20:12 +0100

    update endpoint for finding a simple course

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3434
 
-const courses =[
+const courses = [
     {
         id: 1,
         name: "chemistry"
@@ -18,20 +18,22 @@ const courses =[
     },
 ]
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.get('/api/courses', (req, res)=>{
-    res.send([1,2,3])
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
 })
-app.get('/api/courses/:id', (req, res)=>{
-    const queryParams = req.query
-    const routeParams = req.params
-    res.send(routeParams)
+
+app.get('/api/courses/:id', (req, res) => {
+  const singleCourse =  courses.find(c => c.id === parseInt(req.params.id))
+   // const routeParams = req.query
+    //const routeParams = req.params
+    res.send(singleCourse)
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("App is running on port: " + port)
 })
